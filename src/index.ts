be3605e905b7d2bc31ac18479bc532dea0a8375a
Soumyajit2000-web <soumyajitd2000@gmail.com>
2024-05-24 02:09:50 +0530

    chore: attempt to send instructions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
-// index.ts
-import express from 'express';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+import { DigitalPins } from "./ts/enum";
 
 const app = express();
+const server = createServer(app);
 const port = process.env.PORT || 3000;
+const hostname: string = "0.0.0.0";
+const io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any> =
+  new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript Express!');
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript Express!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.get("/send-message", (req, res) => {
+  io.emit("message", "Hi from server");
+  res.send("Message should be sent!");
+});
+
+app.get("/digital", (req, res) => {
+  const payload = { pin: DigitalPins.D1, status: 1 };
+  const stringifiedPayload = JSON.stringify(payload)
+  io.emit("digital", stringifiedPayload);
+  res.send("Digital pin updated!");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to a user.");
+  socket.on("ping", (msg) => {
+    console.log("Ping message: ", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen({ host: hostname, port: port }, () => {
+  console.log(`server running at http://localhost:${port}`);
+  console.log(`server running at http://192.168.0.101:${port}`);
 });
