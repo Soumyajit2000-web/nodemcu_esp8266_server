b893378a16b886778156c45644b24fb3f0dab1f5
Soumyajit2000-web <soumyajitd2000@gmail.com>
2024-05-25 02:34:23 +0530

    chore: pin error message update

--- src/modules/digital-module/dto/DigitalPinDTO.ts
+++ src/modules/digital-module/dto/DigitalPinDTO.ts
@@ -3,7 +3,7 @@ import { DigitalPins } from "../../../ts/enum";
 
 export class DigitalPinDTO {
   @IsEnum(DigitalPins, {
-    message: "pin must be a valid DigitalPin enum value",
+    message: "pin must be a valid DigitalPin enum value. It must be one these: 16 -> D0, 5 -> D1, 4 -> D2, 0 -> D3, 2 -> D4, 14 -> D5, 12 -> D6, 13 -> D7, 15 -> D8",
   })
   @IsNotEmpty()
   pin: DigitalPins;
