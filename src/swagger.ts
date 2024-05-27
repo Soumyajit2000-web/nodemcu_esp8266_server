203c5b8a2729413a67ac29a3613c1dfb209e36ef
Soumyajit2000-web <soumyajitd2000@gmail.com>
2024-05-27 09:30:52 +0530

    chore: attempt 2 to fix swagger docs

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,23 +1,23 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'API Documentation',
+      title: 'ESP 8266 Control APIs',
       version: '1.0.0',
-      description: 'API documentation for your application',
+      description: '',
     },
-    servers: [
-      {
-        url: process.env.SERVER_URL || 'http://localhost:3000',
-      },
-    ],
+    // servers: [
+    //   {
+    //     url: process.env.SERVER_URL || 'http://localhost:3000',
+    //   },
+    // ],
   },
   apis: ['./src/modules/digital-module/*.ts'], // Path to the API docs. Add the paths in the array for new modules
 };
