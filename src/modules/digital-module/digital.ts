cf3770f887c838e2d1f01c13c7501bc3b0ce3f9a
Soumyajit2000-web <soumyajitd2000@gmail.com>
2024-05-27 08:01:08 +0530

    add: swagger implementation

--- src/modules/digital-module/digital.ts
+++ src/modules/digital-module/digital.ts
@@ -7,12 +7,45 @@ const router = Router();
 
 export default (io: Server) => {
   //This endpoint is for testing purpose
+  /**
+   * @swagger
+   * /send-message:
+   *   get:
+   *     summary: Send a test message
+   *     responses:
+   *       200:
+   *         description: Message should be sent!
+   */
   router.use("/send-message", (req, res) => {
     io.emit("message", "Hi from server");
     res.send("Message should be sent!");
   });
 
   //This endpoint is for controlling digital pins
+  /**
+   * @swagger
+   * /digital:
+   *   post:
+   *     summary: Control digital pins
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               pin:
+   *                 type: integer
+   *                 example: 16
+   *               status:
+   *                 type: integer
+   *                 example: 1
+   *     responses:
+   *       200:
+   *         description: Digital pin updated!
+   *       400:
+   *         description: Validation error
+   */
   router.post("/digital", validationMiddleware(DigitalPinDTO), (req, res) => {
     const payload = { pin: req.body.pin, status: req.body.status };
     const stringifiedPayload = JSON.stringify(payload);
